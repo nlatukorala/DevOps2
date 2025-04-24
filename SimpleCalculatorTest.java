3744eaab9f643c7cbc50c7df13dadfe77f50e987
nlatukorala <87771480+nlatukorala@users.noreply.github.com>
2025-04-24 06:10:17 -0500

    Create SimpleCalculatorTest.java

---
+++ SimpleCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleCalculatorTest {
+
+    @Test
+    public void testAddition() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
