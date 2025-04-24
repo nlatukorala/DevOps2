3a605eca238b266b28902723eea99a5d4071cacf
nlatukorala <87771480+nlatukorala@users.noreply.github.com>
2025-04-24 08:46:03 -0500

    Update SimpleCalculatorTest.java

--- SimpleCalculatorTest.java
+++ SimpleCalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleCalculatorTest {
 
     @Test
-    public void testAddition() {
+    public void testAdd() {
         SimpleCalculator calculator = new SimpleCalculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result);
