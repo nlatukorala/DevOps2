3377cb0f4d26b8a3affefb583ac450ef1ae1b555
nlatukorala <87771480+nlatukorala@users.noreply.github.com>
2025-04-24 06:08:19 -0500

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,11 @@
+public class SimpleCalculator{
+    public int add(int x,int y){
+        return x+y;
+    }
+    public int sub(int x,int y){
+        return x-y;
+    }
+    public int mul(int x,int y){
+        return x*y;
+    }
+}
