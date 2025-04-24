64b1919f2295923179b7b84456d2ae49105c4251
nlatukorala <87771480+nlatukorala@users.noreply.github.com>
2025-04-24 08:57:40 -0500

    Update SimpleCalculatorTest.java

--- SimpleCalculatorTest.java
+++ SimpleCalculatorTest.java
@@ -1,12 +1,20 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class SimpleCalculatorTest {
 
-    @Test
-    public void testAdd() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        int result = calculator.add(2, 3);
-        assertEquals(5, result);
+    SimpleCalculator calculator = new SimpleCalculator();
+    
+    public static String testAdd(int x, int y, int expected) {
+        int result = calculator.add(x, y);
+        if(result == expected){
+            return "Passed";
+        }
+        return "Failed";
+    }
+
+    public static void main(){
+        System.out.println("Testing SimpleCalculator");
+        System.out.println("---------------------------");
+        System.out.println("Testing add() method");
+        System.out.println("add(2,3):"+testAdd(2,3,5));
+        System.out.println("add(10,5):"+testAdd(10,5,15));
     }
 }
